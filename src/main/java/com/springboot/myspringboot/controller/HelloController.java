0fa578a1892101878ebe0cb27a193fbf1e308620
wolfwell5 <416129082@qq.com>
2020-02-08 18:53:12 +0800

    test

--- src/main/java/com/springboot/myspringboot/controller/HelloController.java
+++ src/main/java/com/springboot/myspringboot/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/test")
 public class HelloController {
-
+// commit
 
     @GetMapping("/hi/{id}")
     @ResponseBody
