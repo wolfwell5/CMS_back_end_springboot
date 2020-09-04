3405170d1d6263abfcf0c44dcb734789dd2e8496
wolfwell5 <416129082@qq.com>
2020-09-04 20:36:38 +0800

    1. 大版本提交

---
+++ src/main/java/com/springboot/myspringboot/config/BaseInterceptor.java
@@ -0,0 +1,10 @@
+//package com.springboot.myspringboot.config;
+//
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//@Component
+//public class BaseInterceptor implements HandlerInterceptor {
+//
+//
+//}
